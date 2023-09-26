9f64ef5469a3818c1822b58b6e760982ca3a856f
avinashmumbai1 <dashaa107@gmail.com>
2023-09-26 14:05:50 +0530

    pushing

--- Program.cs
+++ Program.cs
@@ -1,25 +1,26 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int[] inputArray = { 1, 2, 3 };
-        int[] reverseArray = FnreverseArray(inputArray);
+﻿//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        int[] inputArray = { 1, 2, 3 };
+//        int[] reverseArray = FnreverseArray(inputArray);
 
-        foreach (int num in reverseArray)
-        {
-            Console.WriteLine(num);
-        }
+//        foreach (int num in reverseArray)
+//        {
+//            Console.WriteLine(num);
+//        }
  
-    static int[] FnreverseArray(int[] arrays)
-        {
-            int[] ints = new int[arrays.Length];
-            int j = 0;
-            for (int i = arrays.Length - 1; i >= 0; i--)
-            {
-                ints[j] = arrays[i];
-                j++;
-            }
-            return ints;
-        }
-    }
-}
+//    static int[] FnreverseArray(int[] arrays)
+//        {
+//            int[] ints = new int[arrays.Length];
+//            int j = 0;
+//            for (int i = arrays.Length - 1; i >= 0; i--)
+//            {
+//                ints[j] = arrays[i];
+//                j++;
+//            }
+//            return ints;
+//        }
+//    }
+//}   
+
