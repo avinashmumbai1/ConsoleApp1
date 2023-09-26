a50ad6d94c83124b449f61f3e9e8d2b5e9a10c2b
avinashmumbai1 <dashaa107@gmail.com>
2023-09-26 12:57:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] inputArray = { 1, 2, 3 };
+        int[] reverseArray = FnreverseArray(inputArray);
+
+        foreach (int num in reverseArray)
+        {
+            Console.WriteLine(num);
+        }
+ 
+    static int[] FnreverseArray(int[] arrays)
+        {
+            int[] ints = new int[arrays.Length];
+            int j = 0;
+            for (int i = arrays.Length - 1; i >= 0; i--)
+            {
+                ints[j] = arrays[i];
+                j++;
+            }
+            return ints;
+        }
+    }
+}
